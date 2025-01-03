c4a7b2ad93f987e4f123f3abec3b87a5804cc86a
nothing36 <prodipsarker953@gmail.com>
2025-01-03 23:43:29 +0600

    first commit

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+
+i=chr
+for i in range(ord('a'),ord('z')+1):
+     print(chr(i),"-",i)
+
+     
---
+++ even-ascii.py
@@ -0,0 +1,4 @@
+i=chr
+for i in range(ord('a'),ord('z')+1):
+    if i%2==0:
+        print(chr(i),"-",i)
